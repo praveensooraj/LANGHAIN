ea8f3304d7e363dd90a8905182f37f699fe83937
PRAVEEN SOORAJ B <95950543+praveensooraj@users.noreply.github.com>
2024-06-19 11:15:41 +0530

    Add files via upload

---
+++ LANGCHAIN/chatbot/app.py
@@ -0,0 +1,41 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Set environment variables
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+# Check if the environment variables are set correctly
+if os.environ["OPENAI_API_KEY"] is None:
+    raise ValueError("OPENAI_API_KEY environment variable is not set")
+if os.environ["LANGCHAIN_API_KEY"] is None:
+    raise ValueError("LANGCHAIN_API_KEY environment variable is not set")
+
+# Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the user queries"),
+        ("user", "Question: {question}")
+    ]
+)
+
+# Streamlit framework
+st.title('Langchain Demo With OPENAI API')
+input_text = st.text_input("Search the topic you want")
+
+# OpenAI LLM
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({'question': input_text}))
